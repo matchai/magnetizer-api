f4dec4a9185fb45aebf6f52a86fe8785b4a2b8c2
Matan Kushner <mkushner@premiumbeat.com>
2016-12-05 12:24:14 -0500

    🚑 Add error handling

--- index.js
+++ index.js
@@ -7,36 +7,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-function getTorrent(url) {
-	return new Promise((resolve, reject) => {
-		request({url, encoding: null}, (err, res, body) => {
-			const statusCode = res.statusCode;
-			const contentType = res.headers['content-type'];
-
-			let error;
-			if (err) {
-				error = new Error(`Request Failed.\n` +
-													`The following error was encountered: ${err}`);
-			} else if (statusCode !== 200) {
-				error = new Error(`Request Failed.\n` +
-													`Status Code: ${statusCode}`);
-			} else if (!/^application\/x-bittorrent/.test(contentType)) {
-				error = new Error(`Invalid content-type.\n` +
-													`Expected application/x-bittorrent but received ${contentType}`);
-			}
-
-			const {name, infoHash, infoHashBuffer, announce} = parseTorrent(body);
-			const magnet = parseTorrent.toMagnetURI({name, infoHash, infoHashBuffer, announce});
-
-			if (error) {
-				reject(error);
-			} else {
-				resolve(magnet);
-			}
-		});
-	});
-}
-
 app.get('/', (req, res) => {
 	res.send('Try sending a url!');
 });
@@ -46,11 +16,51 @@ app.get('/*', (req, res) => {
 	if (validUrl.isUri(url)) {
 		// URL was sent as param
 		getTorrent(url)
-			.then(out => res.send(out));
+			.then(magnet => {
+				res.json({
+					magnet
+				})
+				res.send(out);
+			})
+			.catch((err) => {
+				console.log(err.stack);
+				res.send(`${err.name}: ${err.message}`);
+			});
 	} else {
 		res.sendStatus(404);
 	}
 });
+
+function getTorrent(url) {
+	return new Promise((resolve, reject) => {
+		request({url, encoding: null, timeout: 1500}, (err, res, body) => {
+			const statusCode = res.statusCode;
+			const contentType = res.headers['content-type'];
+
+			if (statusCode !== 200) {
+				return reject(errorHandler('Request Failed', `Status Code: ${statusCode}`));
+			} else if (!/^application\/x-bittorrent/.test(contentType)) {
+				return reject(errorHandler('Invalid content-type', `Expected application/x-bittorrent but received ${contentType}`));
+			} else if (err) {
+				return reject(errorHandler('Request Failed', `The following error was encountered: ${err}`));
+			}
+
+			const {name, infoHash, infoHashBuffer, announce} = parseTorrent(body);
+			const magnet = parseTorrent.toMagnetURI({name, infoHash, infoHashBuffer, announce});
+
+			return resolve(magnet);
+		});
+	});
+}
+
+function errorHandler(name, message) {
+	const stack = new Error(`${name} - ${message}`);
+	return {
+		name,
+		message,
+		stack
+	}
+}
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}.`);
